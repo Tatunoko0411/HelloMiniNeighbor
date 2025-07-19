545fb23e81f1cf3284b046c0c30fabe9a39ad833
Tatunoko0411 <241202@st.yoshida-g.ac.jp>
2025-07-19 18:14:49 +0900

    タイトルの追加

---
+++ Assets/Scripts/StageSelect/StageSelectManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageSelectManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void BackTitle()
+    {
+        Initiate.Fade("TitleScene",Color.black,1.0f);
+    }
+
+    public void SelectStage(int stage)
+    {
+        Initiate.Fade("GameScene", Color.black, 1.0f);
+
+    }
+}
